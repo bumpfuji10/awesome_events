f4cecf6b2f17d8f1d6208d45bd91ca78e8f69aef
Yo Kamada <bumpfuji10@gmail.com>
2023-10-22 11:11:35 +0900

    add: eventsコントローラなどを生成

---
+++ db/migrate/20231022021103_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.bigint :owner_id
+      t.string :name
+      t.string :place
+      t.datetime :start_at
+      t.datetime :end_at
+
+      t.timestamps
+    end
+  end
+end

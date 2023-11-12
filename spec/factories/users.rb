c9d6d8e601eb42aa1219c6469581316fbc2e589d
Yo Kamada <bumpfuji10@gmail.com>
2023-11-12 17:05:23 +0900

    uidをsequenceせずにtestUid固定にした。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user, aliases: [:owner] do
     provider { "github" }
-    sequence(:uid) { |i| "uid#{i}" }
+    uid { "testUid" }
     sequence(:name) { |i| "name#{i}" }
     sequence(:image_url) { |i| "http://example.com/image#{i}.jpg"}
   end

b5f896090767bd03931d499df24303c429c03b7f
Yo Kamada <bumpfuji10@gmail.com>
2023-12-02 15:56:30 +0900

    wip: モックを使ったテスト

--- spec/models/forms/event_search_form_spec.rb
+++ spec/models/forms/event_search_form_spec.rb
@@ -1,11 +1,44 @@
 require "rails_helper"
 
 RSpec.describe EventSearchForm, type: :model do
-  describe "test" do
-    context "test" do
-      it "test" do
-        pp "test"
+  describe "#search" do
+    let!(:event_creator) { FactoryBot.create(:user) }
+    let!(:event_A) { FactoryBot.create(:event, owner: event_creator, name: "good event") }
+    let!(:event_B) { FactoryBot.create(:event, owner: event_creator, name: "bad event") }
+    let!(:event_C) { FactoryBot.create(:event, owner: event_creator, name: "ng event") }
+
+    let!(:called_event) { [event_A] }
+
+      it "イベントの呼び出しに成功すること" do
+        Event.reindex
+        Event.search_index.refresh
+
+        form = EventSearchForm.new(keyword: "good event")
+        expect(Event).to receive(:search).with('good event', anything).and_return(called_event)
+
+        result = form.search
+        expect(result).to match_array(called_event)
+
+      end
+
+    context "特定のキーワードで検索検索する場合" do
+      let!(:search_using_upcoming_event) { EventSearchForm.new(keyword: "", start_at: Time.now, page: nil) }
+      let!(:upcoming_event) { FactoryBot.create(:event, owner: event_creator, name: "upcoming event", start_at: Time.now + 4.days, end_at: Time.now + 5.days) }
+      let!(:past_event) { FactoryBot.create(:event, owner: event_creator, name: "past_event", start_at: Time.now - 1.days) }
+
+      context "日付範囲を指定して検索する場合" do
+        it "指定された日付範囲を持つイベントのみを返却すること" do
+          Event.reindex
+          Event.search_index.refresh
+
+          form = EventSearchForm.new(keyword: "", start_at: Time.now, page: nil)
+          results = form.search.results
+          pp results
+          pp Event.all
+        end
       end
     end
   end
 end
+
+# {"keyword"=>"fff", "start_at"=>"2023-10-01T00:20:13", "page"=>nil}

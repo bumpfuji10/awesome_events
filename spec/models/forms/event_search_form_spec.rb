568e79341c4779a0f79420e2aea5ceaaa77264c8
Yo Kamada <bumpfuji10@gmail.com>
2023-12-02 12:30:38 +0900

    event_search_formモデルのspecファイルを作成

---
+++ spec/models/forms/event_search_form_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe EventSearchForm, type: :model do
+  describe "test" do
+    context "test" do
+      it "test" do
+        pp "test"
+      end
+    end
+  end
+end
